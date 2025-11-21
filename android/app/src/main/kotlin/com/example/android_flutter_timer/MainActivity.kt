c501d5e3f60f0fcb5eaa1d7850c0c3d7719db511
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-11-21 17:12:26 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/android_flutter_timer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.android_flutter_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+        title: Text("Flutter Timer"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ]
+        ),
+      ),
+    );
+  }
+}
